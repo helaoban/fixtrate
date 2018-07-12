6c1c4cd325c7a87ff6343fcf0f02d345bfa04f14
Carlo Holl <carloholl@gmail.com>
2018-07-12 16:52:55 -0700

    Improve RPC Logic
    - Add RPCClient class for easy interface to server
    - Pass a fix client (user implementent) to RPCServer instead of session
    - Remove anything that would be considered client code

--- fixation/rpc.py
+++ fixation/rpc.py
@@ -5,8 +5,10 @@ import jsonschema as js
 import logging
 import os
 import socket
+import time
+import uuid
 
-from fixation import exceptions, utils
+from fixation import constants as fc, exceptions, utils
 
 
 logger = logging.getLogger(__name__)
@@ -30,9 +32,9 @@ class RPCServer(object):
         ]
     }
 
-    def __init__(self, fix_session, loop=None):
+    def __init__(self, fix_client, loop=None):
         self.config_path = '~/.fixation'
-        self.session = fix_session
+        self.fix_client = fix_client
         self._socket_server = None
         self.loop = loop or asyncio.get_event_loop()
 
@@ -49,21 +51,20 @@ class RPCServer(object):
     def make_socket_file(self):
         config_dir = self.make_config_dir()
         path = os.path.join(config_dir, 'command_socket')
-
         try:
             os.remove(path)
         except OSError:
             pass
-
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         s.bind(path)
-
-        return path
+        return s
 
     def dispatch_socket_command(self, data):
         return {
-            'place_order': self.session.place_order,
-            'send_test_request': self.session.send_test_request
+            'place_order': self.fix_client.place_order,
+            'cancel_order': self.fix_client.cancel_order,
+            'cance_replace_order': self.fix_client.cancel_replace_order,
+            'send_test_request': self.fix_client.send_test_request
         }.get(data['method'])
 
     async def handle_rpc_request(self, message):
@@ -77,7 +78,7 @@ class RPCServer(object):
         except js.ValidationError as error:
             raise exceptions.RPCInvalidRequest
 
-        handler = await self.dispatch_socket_command(data)
+        handler = self.dispatch_socket_command(data)
         if not handler:
             raise exceptions.RPCMethodNotFound
 
@@ -96,7 +97,7 @@ class RPCServer(object):
             'id': data['id']
         }
 
-    async def handle_socket_client(self, reader, writer, timeout=30):
+    async def handle_socket_client(self, reader, writer, timeout=10):
         buf = b''
         while True:
             try:
@@ -104,7 +105,7 @@ class RPCServer(object):
             except asyncio.TimeoutError as error:
                 logger.exception(error)
                 return
-            if buf == b'0':
+            if buf == b'':
                 logger.info('client disconnected')
                 writer.close()
                 return
@@ -129,9 +130,84 @@ class RPCServer(object):
         await self._socket_server.wait_close()
 
     async def start(self):
-        socket_path = self.make_socket_file()
-        self._socket_server = asyncio.start_unix_server(
+        sock = self.make_socket_file()
+        self._socket_server = await asyncio.start_unix_server(
             self.handle_socket_client,
-            path=socket_path,
+            sock=sock,
             loop=self.loop
         )
+
+
+class RPCClient:
+
+    class CouldNotConnectError(Exception): pass
+    class BadConnectionError(Exception): pass
+    class CommandError(Exception): pass
+
+    def __init__(self, timeout=5):
+        self.socket = socket.socket(
+            socket.AF_UNIX,
+            socket.SOCK_STREAM
+        )
+        self.socket.settimeout(timeout)
+        try:
+            self.socket.connect(
+                os.path.expanduser(
+                    '~/.fixation/command_socket'
+                )
+            )
+        except socket.error:
+            raise self.CouldNotConnectError
+        self.socket_file = self.socket.makefile('rwb', 4096)
+
+    def close(self):
+        self.socket_file.close()
+        self.socket.close()
+
+    def read(self):
+        try:
+            r = self.socket_file.readline().decode().rstrip('\n')
+        except socket.error:
+            raise self.BadConnectionError
+
+        if r == '':
+            raise EOFError
+
+        return r
+
+    def send_command(self, name, timeout=30, **kwargs):
+
+        uid = uuid.uuid4()
+        message = {
+            'jsonrpc': '2.0',
+            'method': name,
+            'params': kwargs,
+            'id': uid
+        }
+        message = utils.pack_rpc_message(message)
+        self.socket_file.write(message)
+        self.socket_file.flush()
+
+        try:
+            buf = b''
+            t = time.time()
+            while time.time() - t < timeout:
+                buf += self.socket_file.read(4096)
+                resp, buf = utils.parse_rpc_message(buf)
+                if resp is not None:
+                    break
+            else:
+                raise TimeoutError
+        except KeyboardInterrupt:
+            raise self.BadConnectionError()
+        except TimeoutError:
+            print('Timeout!')
+            raise
+        except Exception as error:
+            logger.exception(error)
+            return
+
+        if 'result' in resp:
+            return resp['result']
+        else:
+            raise self.CommandError(resp['error'])
