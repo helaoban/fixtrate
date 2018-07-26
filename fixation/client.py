f3c30a5ddaa88fcb713b1ab8490db7ae328ccd4e
Carlo Holl <carloholl@gmail.com>
2018-07-26 15:41:05 -0700

    Cleanup

--- fixation/client.py
+++ fixation/client.py
@@ -14,22 +14,17 @@ from fixation.factories import fix42
 class FixClient(object):
 
     def __init__(self, conf=None):
-        self.session = None
-        self.rpc_server = None
-        self.loop = asyncio.get_event_loop()
-
-        if conf is None:
-            conf = config.get_config_from_env()
-        else:
-            config.validate_config(conf)
-
-        self.config = conf
-
-        self.TAGS = getattr(fc.FixTag, self.config['FIX_VERSION'].name)
+        conf = conf or config.get_config_from_env()
+        config.validate_config(conf)
+        self._config = conf
+        self._session = None
+        self._rpc_server = None
+        self._loop = asyncio.get_event_loop()
+        self._tags = getattr(fc.FixTag, self._config['FIX_VERSION'].name)
 
     async def send_test_request(self):
         msg = fix42.test_request()
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     async def place_order(
             self,
@@ -42,7 +37,7 @@ class FixClient(object):
             ex_destination='SMART',
             time_in_force=None
     ):
-        account = self.config['FIX_ACCOUNT']
+        account = self._config['FIX_ACCOUNT']
         if time_in_force is not None:
             time_in_force = fc.TimeInForce[time_in_force]
         msg = fix42.new_order(
@@ -56,7 +51,7 @@ class FixClient(object):
             ex_destination=ex_destination,
             time_in_force=time_in_force
         )
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     async def cancel_replace_order(
             self,
@@ -67,7 +62,7 @@ class FixClient(object):
             order_type,
             price=None,
     ):
-        account = self.config['FIX_ACCOUNT']
+        account = self._config['FIX_ACCOUNT']
         msg = fix42.cancel_replace(
             account=account,
             orig_cl_order_id=order_id,
@@ -77,7 +72,7 @@ class FixClient(object):
             quantity=quantity,
             price=price
         )
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     async def cancel_order(
         self,
@@ -86,7 +81,7 @@ class FixClient(object):
         side,
         quantity
     ):
-        account = self.config['FIX_ACCOUNT']
+        account = self._config['FIX_ACCOUNT']
         msg = fix42.cancel(
             account=account,
             orig_cl_order_id=order_id,
@@ -94,7 +89,7 @@ class FixClient(object):
             side=fc.Side[side],
             quantity=quantity
         )
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     async def order_status(
         self,
@@ -103,13 +98,13 @@ class FixClient(object):
         msg = fix42.order_status(
             cl_order_id=order_id,
         )
-        account = self.config['FIX_ACCOUNT']
-        msg.append_pair(self.TAGS.Account, account)
-        await self.session.send_message(msg)
+        account = self._config['FIX_ACCOUNT']
+        msg.append_pair(self._tags.Account, account)
+        await self._session.send_message(msg)
 
     async def get_security_list(self):
         msg = fix42.security_list()
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     async def request_market_data(self, symbols):
         """
@@ -128,7 +123,7 @@ class FixClient(object):
         msg = fix42.market_data_request(
             symbols, entry_types
         )
-        await self.session.send_message(msg)
+        await self._session.send_message(msg)
 
     def handle_market_data_full_refresh(self, msg):
         market_request_id = msg.get(262)
@@ -140,15 +135,15 @@ class FixClient(object):
         book = collections.defaultdict(list)
         trades = []
         for i in range(number_of_entries):
-            entry_type = msg.get(self.TAGS.MDEntryType)
-            price = msg.get(self.TAGS.MDEntryPx)
-            size = msg.get(self.TAGS.MDEntrySize)
+            entry_type = msg.get(self._tags.MDEntryType)
+            price = msg.get(self._tags.MDEntryPx)
+            size = msg.get(self._tags.MDEntrySize)
 
             if entry_type in [
                 fc.MDEntryType.OFFER,
                 fc.MDEntryType.BID
             ]:
-                book[self.TAGS.MDEntryType] = {
+                book[self._tags.MDEntryType] = {
                     'price': price,
                     'size': size
                 }
@@ -181,7 +176,7 @@ class FixClient(object):
         return handler
 
     async def handle_message(self, msg):
-        msg_type = fc.FixMsgType(msg.get(self.TAGS.MsgType))
+        msg_type = fc.FixMsgType(msg.get(self._tags.MsgType))
         handler = self.dispatch_message(msg_type)
         if handler:
             if utils.is_coro(handler):
@@ -194,50 +189,50 @@ class FixClient(object):
         redis_pool = await aioredis.create_redis_pool(
             'redis://localhost', minsize=5, maxsize=10)
 
-        store = fs.FixRedisStore(redis_pool, self.config)
+        store = fs.FixRedisStore(redis_pool, self._config)
 
-        self.session = session.FixSession(
-            conf=self.config,
+        self._session = session.FixSession(
+            conf=self._config,
             store=store,
-            loop=self.loop,
+            loop=self._loop,
         )
-        self.rpc_server = rpc.RPCServer(
+        self._rpc_server = rpc.RPCServer(
             self,
-            loop=self.loop
+            loop=self._loop
         )
 
-        @self.session.on_recv_message
+        @self._session.on_recv_message
         def print_incoming_to_console(msg):
-            send_time = msg.get(self.TAGS.SendingTime)
+            send_time = msg.get(self._tags.SendingTime)
             print('{}: {} <--'.format(send_time, msg))
 
-        @self.session.on_send_message
+        @self._session.on_send_message
         def print_outgoing_to_console(msg):
-            send_time = msg.get(self.TAGS.SendingTime)
+            send_time = msg.get(self._tags.SendingTime)
             print('{}: {} -->'.format(send_time, msg))
 
         while True:
             try:
-                async with self.session.connect():
-                    await self.session.logon()
-                    await self.rpc_server.start()
+                async with self._session.connect():
+                    await self._session.logon()
+                    await self._rpc_server.start()
 
-                    async for msg in self.session:
+                    async for msg in self._session:
                         await self.handle_message(msg)
 
-                    await self.session.logoff()
+                    await self._session.logoff()
 
             except ConnectionError:
                 continue
             except asyncio.CancelledError:
                 break
 
-        await self.rpc_server.stop()
+        await self._rpc_server.stop()
 
     def __call__(self):
-        task = self.loop.create_task(self.main())
+        task = self._loop.create_task(self.main())
         try:
-            self.loop.run_until_complete(task)
+            self._loop.run_until_complete(task)
         except (SystemExit, KeyboardInterrupt):
             task.cancel()
-            self.loop.run_until_complete(task)
+            self._loop.run_until_complete(task)
