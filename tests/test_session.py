80ff5190d93a16db647548996640457d909e30e7
Carlo Holl <carloholl@gmail.com>
2018-07-29 18:37:45 -0700

    Shore up session tests

--- tests/test_session.py
+++ tests/test_session.py
@@ -64,26 +64,22 @@ async def test_heartbeat(fix_session, test_server):
 
 @pytest.mark.asyncio
 async def test_incorrect_heartbeat_int(fix_session, test_server):
-    fix_session._config['FIX_HEARTBEAT_INTERVAL'] = 90
+    fix_session._config['HEARTBEAT_INTERVAL'] = 90
+
     async with fix_session.connect():
         await fix_session.logon()
-        async for msg in fix_session:
-            assert msg.msg_type == fc.FixMsgType.Reject
-            break
-        else:
-            raise AssertionError('No message received')
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Reject
 
 
 @pytest.mark.asyncio
 async def test_incorrect_target_comp_id(fix_session, test_server):
-    fix_session._config['FIX_TARGET_COMP_ID'] = 'not-a-correct-id'
+    fix_session._config['TARGET_COMP_ID'] = 'not-a-correct-id'
+
     async with fix_session.connect():
         await fix_session.logon()
-        async for msg in fix_session:
-            assert msg.msg_type == fc.FixMsgType.Reject
-            break
-        else:
-            raise AssertionError('No message received')
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Reject
 
 
 @pytest.mark.asyncio
@@ -93,30 +89,22 @@ async def test_new_seq_num(fix_session, test_server):
 
     async with fix_session.connect():
         await fix_session.logon()
-        async for msg in fix_session:
-            assert msg.msg_type == fc.FixMsgType.Logon
-            break
-        else:
-            AssertionError('No message received')
+
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Logon
 
         await fix_session._send_test_request(test_id)
-        async for msg in fix_session:
-            if msg.msg_type == fc.FixMsgType.Heartbeat:
-                if msg.get(112) == test_id:
-                    break
-        else:
-            raise AssertionError('No message received')
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Heartbeat
+        assert msg.get(112) == test_id
 
         await fix_session.logon(reset=True)
-        async for msg in fix_session:
-            assert msg.msg_type == fc.FixMsgType.Logon
-            assert msg.get(TAGS.ResetSeqNumFlag) == fc.ResetSeqNumFlag.YES
-            assert msg.seq_num == 1
-            stored_seq_num = await fix_session._store.get_seq_num()
-            assert stored_seq_num == 2
-            break
-        else:
-            raise AssertionError('No message received')
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Logon
+        assert msg.get(TAGS.ResetSeqNumFlag) == fc.ResetSeqNumFlag.YES
+        assert msg.seq_num == 1
+        stored_seq_num = await fix_session._store.get_seq_num()
+        assert stored_seq_num == 2
 
 
 @pytest.mark.asyncio
@@ -127,12 +115,13 @@ async def test_test_request(fix_session, test_server):
     async with fix_session.connect():
         await fix_session.logon()
         await fix_session._send_test_request(test_id)
-        async for msg in fix_session:
-            if msg.msg_type == fc.FixMsgType.Heartbeat:
-                if msg.get(112) == test_id:
-                    break
-        else:
-            raise AssertionError('No message received')
+
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Logon
+
+        msg = await fix_session.receive()
+        assert msg.msg_type == fc.FixMsgType.Heartbeat
+        assert msg.get(112) == test_id
 
 
 @pytest.mark.asyncio
