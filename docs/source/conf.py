e9c1fd9e929fb163179d5ff866895b634065e27e
Carlo Holl <carloholl@gmail.com>
2018-07-31 15:50:07 -0700

    Add asyncio enxtension in sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.asyncio',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
