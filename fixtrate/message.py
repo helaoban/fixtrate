3feb52393e7b977af54103ae26fed4820c0e059b
Carlo Holl <carloholl@gmail.com>
2019-01-25 18:00:09 -0800

    Remove explicit checks for enums
    All lib enums now inherit from basic datatypes, so equality
    assertions will work without needing to reference Enum.value.

--- fixtrate/message.py
+++ fixtrate/message.py
@@ -6,47 +6,6 @@ import simplefix
 from . import utils, constants as fc
 
 
-@utils.monkeypatch_module(simplefix.message)
-def fix_val(value):
-
-    # Enum support
-    try:
-        value = value.value
-    except AttributeError:
-        pass
-
-    if isinstance(value, (bytes, str,)):
-        if len(value) < 1:
-            raise ValueError('FIX value cannot be empty!')
-        try:
-            value = value.encode()
-        except AttributeError:
-            pass
-        return value
-    else:
-        return bytes(str(value), 'ASCII')
-
-
-@utils.monkeypatch_module(simplefix.message)
-def fix_tag(value):
-    """Make a FIX tag value from string, bytes, integer, or Enum"""
-
-    # Enum support
-    try:
-        value = value.value
-    except AttributeError:
-        pass
-
-    if sys.version_info[0] == 2:
-        return bytes(value)
-    else:
-        if type(value) == bytes:
-            return value
-        elif type(value) == str:
-            return value.encode('ASCII')
-        return str(value).encode('ASCII')
-
-
 class FixMessage(simplefix.FixMessage):
 
     def __init__(self, uid=None):
@@ -112,11 +71,7 @@ class FixMessage(simplefix.FixMessage):
 
         :return: bool
         """
-        poss_dup_flag = self.get(43)
-        if poss_dup_flag is None:
-            return False
-        poss_dup_flag = fc.PossDupFlag(poss_dup_flag)
-        return poss_dup_flag == fc.PossDupFlag.YES
+        return self.get(43) == fc.PossDupFlag.YES
 
     def get(self, tag, nth=1, raw=False):
         val = super().get(tag, nth=nth)
