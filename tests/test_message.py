b5c6accb97cbe91e7e0bfeeee399b1bc4d819f30
Carlo Holl <carloholl@gmail.com>
2019-02-25 16:02:11 -0800

    Remove old tests

--- tests/test_message.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from fixtrate import constants as fc, message
-
-
-def test_reject_empty_value():
-    msg = message.FixMessage()
-    with pytest.raises(ValueError):
-        msg.append_pair(fc.FixTag.FIX44.Text, b'')
